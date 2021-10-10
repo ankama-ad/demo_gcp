2d155a33cc7c430c464242e5d78b2f2daf415191
ankama-ad <ankamma.bollimuntha@amnetdigital.com>
2021-10-10 21:49:16 +0530

    removed creds

--- angular-10-firestore-crud-master/src/environments/environment.ts
+++ angular-10-firestore-crud-master/src/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCr3oZJB3rTNWZcgVEYiNJFlGjtGSfUn20",
-    authDomain: "ng-demo-gcp-gae.firebaseapp.com",
-    projectId: "ng-demo-gcp-gae",
-    storageBucket: "ng-demo-gcp-gae.appspot.com",
-    messagingSenderId: "106654260853",
-    appId: "1:106654260853:web:69069ceafe98b9a937223c",
-    measurementId: "G-0ZBY619RYL"
+    
   }
 };
